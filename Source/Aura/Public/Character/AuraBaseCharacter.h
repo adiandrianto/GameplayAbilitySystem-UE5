9b9ee4010677b632873374a44a9bdea007cae454
Adi Andrianto <adiandrianto@gmail.com>
2024-10-01 19:01:05 +0700

    character setup

---
+++ Source/Aura/Public/Character/AuraBaseCharacter.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraBaseCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraBaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+};
